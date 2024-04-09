4e199ac8d86a1c709486f18d019ead4f61826a3d
FlyDoggys <138303871+FlyDoggys@users.noreply.github.com>
2024-04-09 10:22:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,47 +1,28 @@
 import discord
 from discord.ext import commands
 
-TOKEN = 'твой токен'
+TOKEN = 'your token'
 
 intents = discord.Intents.default()
-intents.message_content = True
-intents.bans = True
 intents.members = True
 
-bot = commands.Bot(command_prefix="*", intents=intents)
+bot = commands.Bot(command_prefix='/', intents=intents)
 
 @bot.event
 async def on_member_join(member):
-    channel = discord.utils.get(member.guild.text_channels, name="TJMS")
+    channel = discord.utils.get(member.guild.text_channels, name="название-канала")
     if channel:
         await channel.send(f"Привет, {member.mention}! Добро пожаловать на сервер!")
+
+@bot.event
+async def on_member_remove(member):
+    channel = discord.utils.get(member.guild.text_channels, name="название-канала")
+    if channel:
+        await channel.send(f"Жаль, что {member.name} покинул сервер.")
 
 @bot.command()
 async def info(ctx):
     await ctx.send('Команды: *msg, *mute, *kick, *ban')
 
-@bot.command()
-async def mute(ctx, member: discord.Member):
-    role = discord.utils.get(ctx.guild.roles, name="Muted")
-    if not role:
-        role = await ctx.guild.create_role(name="Muted")
-        for channel in ctx.guild.channels:
-            await channel.set_permissions(role, send_messages=False)
-    await member.add_roles(role)
-    await ctx.send(f"{member.mention} теперь в муте.")
-
-@bot.command()
-async def kick(ctx, member: discord.Member):
-    await member.kick()
-    await ctx.send(f"{member.mention} был кикнут.")
-
-@bot.command()
-async def ban(ctx, member: discord.Member):
-    await member.ban()
-    await ctx.send(f"{member.mention} был забанен.")
-
-@bot.command()
-async def msg(ctx):
-    await ctx.send('Привет, мир!')
 
 bot.run(TOKEN)
