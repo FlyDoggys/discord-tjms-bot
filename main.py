c4c0cd9ce6c15ec8cb664f88be89c7019c053271
FlyDoggys <138303871+FlyDoggys@users.noreply.github.com>
2024-04-09 08:44:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
 import discord
+from discord.ext import commands
 
+TOKEN = 'your token'
+
+intents = discord.Intents.default() # Подключаем "Разрешения"
+intents.message_content = True
+# Задаём префикс и интенты
+bot = commands.Bot(command_prefix='/', intents=intents)
+
+# С помощью декоратора создаём первую команду
+@bot.command()
+async def info(ctx):
+    await ctx.send('Commands: msg')
+
+async def message(ctx):
+    await ctx.send('Hello, world!')
+
+bot.run(TOKEN)
